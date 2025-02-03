8e99275fda657b3cbfc8c2f09de0df04b8de8ee3
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2025-02-03 11:59:52 +0500

    Стр. 419

--- app/src/main/java/com/hfad/bitsandpizzas/OrderFragment.kt
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -13,6 +15,17 @@ class OrderFragment : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_order, container, false)
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+        val pizzaType = pizzaGroup.checkedRadioButtonId
+        if (pizzaType == -1) {
+            //Выбранного элемента нет
+        }
+        else {
+            val radio = view.findViewById<RadioButton>(id)
+            //Сделать что-то с переключателем
+        }
+
         return view
     }
 }
